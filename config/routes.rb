57b139f64b57d485ffcaca2ea9308109c5d936b1
amorsillo <attunezero@gmail.com>
2011-03-30 16:20:27 -0400

    added buncha crap from chapt 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 SampleApp::Application.routes.draw do
+  root :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+
   get "pages/home"
   get "pages/about"
   get "pages/contact"
